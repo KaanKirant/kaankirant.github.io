1847b1bb72d3d1023df42f3ef952ba756836f7e0
Kaan Kırant <kaankirant98@gmail.com>
2026-05-17 19:23:29 +0300

    update

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ import { AboutMeData, Game, GameEngine, LinkImageSource, MediaType, Platform } f
 export const PersonalInfo: AboutMeData = {
   name: "Kaan Kirant",
   role: "Game Developer",
-  introduction: "Hi, I'm Kaan, a passionate game developer & AI Enthusiast",
+  introduction: "I’m a game developer passionate about creating engaging gameplay and exploring how AI can make games smarter and more immersive.",
   description: "Hi! My name is Kaan. Dedicated and accomplished software and game developer with a solid background in artificial intelligence. With a Master's in Artificial Intelligence & Data Science and a Bachelor's in Computer Engineering, I provide a combination of technical expertise and original problem-solving skills. Having worked successfully in team settings in the past, I have expertise in creating video games and software applications utilising C++, C# and Python.",
   image: `${process.env.PUBLIC_URL}/images/Profile.jpg`,
   cvUri: `${process.env.PUBLIC_URL}/files/KaanKirant_CV_July2025.pdf`,
@@ -15,22 +15,6 @@ export const PersonalInfo: AboutMeData = {
 };
 
 export const games: Game[] = [
-  {
-    name: "Slash",
-    description: "Learned Unreal Engine 5's Open World maps, Quixel Bridge megascans for ultra-realistic environments, landscape sculpting and painting, dungeons, free-roaming creatures and humanoids with various weapons, breakable objects that spawn treasure. ",
-    genres: ["Open World", "Third-Person"],
-    platforms: [Platform.Windows],
-    engine: GameEngine.Unreal,
-    links: [
-      { source: LinkImageSource.Github, url: "https://github.com/KaanKirant/Slash" },
-    ],
-    media: [
-      { source: "/images/games/Slash/Screenshot_1.png", type: MediaType.Image },
-      { source: "https://www.youtube.com/embed/zL3rDbIOlHk", type: MediaType.YouTube },
-      { source: "/images/games/Slash/Screenshot_2.png", type: MediaType.Image },
-      { source: "/images/games/Slash/Screenshot_3.png", type: MediaType.Image },
-    ],
-  },
   {
     name: "Escape The House",
     description: "A first person game and the goal of the game is to escape the room by solving the puzzles.",
@@ -68,6 +52,23 @@ export const games: Game[] = [
       { source: "/images/games/TowerDefence/Screenshot_3.png", type: MediaType.Image },
     ],
   },
+    {
+    name: "Kitchen Chaos",
+    description: "Pick up some ingredients, prepare them, put them on a plate and deliver them. Work with the various counters to prepare them. Pick up a full Cheese block, cut it into slices, then cook a Meat Patty (but don't let it burn!), pick them up on a Plate, add some Bread and you have a nice burger!",
+    genres: ["3D", "Kitchen", "Food"],
+    platforms: [Platform.Windows],
+    engine: GameEngine.Unity,
+    links: [
+{ source: LinkImageSource.Github, url: "https://github.com/KaanKirant/KitchenChaos" }
+    ],
+    media: [
+      { source: "/images/games/KitchenChaos/Screenshot_2.jpg", type: MediaType.Image },
+      { source: "/images/games/KitchenChaos/Screenshot_3.jpg", type: MediaType.Image },
+      { source: "/images/games/KitchenChaos/Screenshot_1.jpg", type: MediaType.Image },
+	{ source: "/images/games/KitchenChaos/Screenshot_4.jpg", type: MediaType.Image }
+    ],
+  },
+
   {
     name: "IBM's Skills Build Game",
     description: "An arcade quiz game that aims to improve interactivity of the IBM's Skills Build website using gamification. This project developed with an IBM mentor and it is for my masters degree.",
@@ -80,7 +81,23 @@ export const games: Game[] = [
     media: [
       { source: "/images/games/IBMsSkillsBuildGame/Screenshot_2.png", type: MediaType.Image },
       { source: "/images/games/IBMsSkillsBuildGame/Screenshot_3.png", type: MediaType.Image },
-      { source: "/images/games/IBMsSkillsBuildGame/Screenshot_1.png", type: MediaType.Image },
+      { source: "/images/games/IBMsSkillsBuildGame/Screenshot_1.png", type: MediaType.Image }
     ],
-  }
+  },
+{
+    name: "Slash",
+    description: "A third-person action RPG created in Unreal Engine 5 with C++, featuring open-world exploration, enemy AI, combat mechanics, breakable objects, and a soul and treasure collection system.",
+    genres: ["Open World", "Third-Person"],
+    platforms: [Platform.Windows],
+    engine: GameEngine.Unreal,
+    links: [
+      { source: LinkImageSource.Github, url: "https://github.com/KaanKirant/Slash" },
+    ],
+    media: [
+      { source: "/images/games/Slash/Screenshot_1.png", type: MediaType.Image },
+      { source: "https://www.youtube.com/embed/zL3rDbIOlHk", type: MediaType.YouTube },
+      { source: "/images/games/Slash/Screenshot_2.png", type: MediaType.Image },
+      { source: "/images/games/Slash/Screenshot_3.png", type: MediaType.Image },
+    ],
+  },
   ];
